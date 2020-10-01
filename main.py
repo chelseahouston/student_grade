9fd984f18cb6f8cccb9e5255e794f1d13d51ab36
Chelsea Houston <chelseahouston29@gmail.com>
2020-10-01 18:22:22 +0000

    Comments

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ def main():
   student_name = input("Enter your Full Name: ")
 
   def mark_grade():
-    # USER INPUT: PREDICTED GRADE % --
-    predicted_grade = input("Enter your predicted grade (%) (numbers only): ")
+    # USER INPUT: TARGET GRADE % --
+    target_grade = input("Enter your target grade (%) (numbers only): ")
 
     # IS INPUT AN INTEGER? --
     try:
-      predicted_grade = int(predicted_grade)
+      target_grade = int(target_grade)
     except ValueError:
       print("Incorrect Value. Please enter your precentages in numbers only.")
       mark_grade()
@@ -28,33 +28,33 @@ def main():
     # GRADE CALCULATOR --
     if grade_percentage <= 30:
       print(f"NAME: {student_name}")
-      print(f"PREDICTED GRADE: {predicted_grade}%") 
+      print(f"TARGET GRADE: {target_grade}%") 
       print("FINAL RESULT: FAIL (0%-29%)")
     if 31 <= grade_percentage <= 49:
       print(f"NAME: {student_name}")
-      print(f"PREDICTED GRADE: {predicted_grade}%") 
+      print(f"TARGET GRADE: {target_grade}%") 
       print("FINAL RESULT: C (30%-49%)")
     if 50 <= grade_percentage <= 69:
       print(f"NAME: {student_name}")
-      print(f"PREDICTED GRADE: {predicted_grade}%") 
+      print(f"TARGET GRADE: {target_grade}%") 
       print("FINAL RESULT: B (50%-69%)")
     if 70 <= grade_percentage <= 84:
       print(f"NAME: {student_name}")
-      print(f"PREDICTED GRADE: {predicted_grade}%") 
+      print(f"TARGET GRADE: {target_grade}%") 
       print("FINAL RESULT: A (60%-84%)")
     if grade_percentage >= 85:
       print(f"NAME: {student_name}")
-      print(f"PREDICTED GRADE: {predicted_grade}%") 
+      print(f"TARGET GRADE: {target_grade}%") 
       print("FINAL RESULT: A* (85%-100%)")
     # END GRADE CALCULATOR --
 
-    # MEET PREDICTED GRADE? --
-    if grade_percentage < predicted_grade:
-      print("Unfortunately, you did not meet your predicted grade.")
-    if grade_percentage == predicted_grade:
-      print("You achieved your Predicted Grade.")
-    if grade_percentage > predicted_grade:
-      print("You did better than expected and achieved above your predicted grade percentage!")
+    # MEET TARGET GRADE? --
+    if grade_percentage < target_grade:
+      print("Unfortunately, you did not meet your target grade.")
+    if grade_percentage == target_grade:
+      print("You achieved your target grade.")
+    if grade_percentage > target_grade:
+      print("You did better than expected and achieved above your target grade percentage!")
 
   mark_grade()
 main()
